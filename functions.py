a2dc991b311192f498f6fdabd8076050f1b1b78d
Dave Stearns <daveajstearns@gmail.com>
2020-04-16 22:03:33 -0400

    final

--- functions.py
+++ functions.py
@@ -13,7 +13,6 @@ def reset_index(data):
 
 def make_value_chart(data, column, x_label, y_label, title, color='PRGn'):
     """Must fill out all arguments EXCEPT color. Color has a default setting."""
-
     fig, ax = plt.subplots(figsize=(14,8))
     ax = sns.barplot(x=list(data[column].value_counts().keys()),
      y=data[column].value_counts(), palette=color)    
@@ -23,3 +22,22 @@ def make_value_chart(data, column, x_label, y_label, title, color='PRGn'):
     plt.ylabel(y_label, size=20)
     plt.title(title, size=26)
     return plt.show()
+
+def rename_column(data, column, new_name):
+    """Quickly renames columns"""
+    data.rename(columns={column: new_name}, inplace=True)
+    return data
+
+def drop(data, columns=[]):
+    """Quickly drops columns"""
+    data.drop(columns=columns, inplace=True)
+    return data
+
+def dummy_up(data, column, prefix):
+    """Quickly makes dummies for the selected column"""
+    new = pd.get_dummies(data[column], prefix=prefix, drop_first=True)
+    return new
+
+def factorize(data, column):
+    new = pd.factorize(data[column])[0] + 1
+    return new
